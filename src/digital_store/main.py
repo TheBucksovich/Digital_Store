980ed0853c03d79c947cf44e5a0f04084ee38885
Ilya Bochkarev <ibochkarev@kamaz.digital>
2025-04-01 11:56:54 +0300

    Добавлены роутеры для user,product,order

--- src/digital_store/main.py
+++ src/digital_store/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from src.digital_store.routers.user import router as user_router
+from src.digital_store.routers.product import router as product_router
+from src.digital_store.routers.order import router as order_router
+
 
 app = FastAPI()
 
-@app.get("/hello")
-def hello():
-    return {"message": "Hello from Digital Store"}
+app.include_router(user_router)
+app.include_router(product_router)
+app.include_router(order_router)
